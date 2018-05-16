55841bc20dbaf15789b12e72dab7acf8292b7834
Hayden Worden <haydenworden@outlook.com>
2018-05-16 17:30:50 +0100

    Code cleaned up

--- webPage/assets/js/main.js
+++ webPage/assets/js/main.js
@@ -1,29 +1,24 @@
 var block = document.querySelectorAll(".blockOverlay");
 var icons = document.querySelectorAll(".icons");
+var iconBox = document.querySelectorAll(".icon-con");
 
-for(var i = 0;i < block.length; i++){
-  block[i].style.background="rgba(0, 0, 0, 0)"
-}
 
-for(var i = 0; i < block.length; i++){
-  block[i].children[0].style.display="none"
-	block[i].addEventListener("mouseover", function(e){
-    if(this.style.background==="rgba(0, 0, 0, 0)"){
-      this.style.background="black"
-      this.children[0].style.display="block";
-      this.children[1].style.display="none";
-      this.children[2].style.color="white";
-      this.children[2].style.marginTop="30px";
 
-    }
-  })
-  block[i].addEventListener("mouseleave", function(e){
-    if(this.style.background==="black"){
-      this.style.background="rgba(0, 0, 0, 0)";
-      this.children[0].style.display="none";
-      this.children[1].style.display="block";
-      this.children[2].style.color="black";
-      this.children[2].style.marginTop="0px";
-    }
+for(var i=0; i < iconBox.length;i++){
+  iconBox[i].children[0].style.visibility="hidden";
+  iconBox[i].addEventListener("mouseover",function(){
+  this.style.background="black"
+  this.children[0].style.visibility="inherit";
+  this.children[1].style.visibility="hidden";
+  this.children[2].style.color="white";
   })
 }
+for(var i=0; i < iconBox.length;i++){
+  iconBox[i].addEventListener("mouseleave",function(){
+    this.style.background="#fff0"
+   this.children[0].style.visibility="hidden";
+   this.children[1].style.visibility="inherit";
+   this.children[2].style.color="black";
+  })
+}
+
