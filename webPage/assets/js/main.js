6404393d8b86618785f298f80b5163c61b2280c2
Hayden Worden <haydenworden@outlook.com>
2018-05-20 21:42:12 +0100

    Grid changed

--- webPage/assets/js/main.js
+++ webPage/assets/js/main.js
@@ -7,7 +7,7 @@ var iconBox = document.querySelectorAll(".icon-con");
 for(var i=0; i < iconBox.length;i++){
   iconBox[i].children[0].style.visibility="hidden";
   iconBox[i].addEventListener("mouseover",function(){
-  this.style.background="black"
+  this.parentElement.style.background="black"
   this.children[0].style.visibility="inherit";
   this.children[1].style.visibility="hidden";
   this.children[2].style.color="white";
@@ -15,7 +15,8 @@ for(var i=0; i < iconBox.length;i++){
 }
 for(var i=0; i < iconBox.length;i++){
   iconBox[i].addEventListener("mouseleave",function(){
-    this.style.background="#fff0"
+  this.style.background="#fff0"
+  this.parentElement.style.background="#fff0"
    this.children[0].style.visibility="hidden";
    this.children[1].style.visibility="inherit";
    this.children[2].style.color="black";
